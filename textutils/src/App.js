4c3fa019cfe70f69f6ea16822389b5e81154ee97
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-04-08 12:13:14 +0545

    Day 6

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,72 +1,57 @@
-//  Change title dynamically
-
-// import logo from './logo.svg';
-
 import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+import TextFormss from "./components/TextFormss";
+import About from "./components/About";
 import Alert from "./components/Alert";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 // import About from "./components/About";
 
-function App() {
-  const [mode , setMode] = useState('light'); //check whether dark mode is enabled or not
 
+function App() {
+  const [mode, setMode] = useState('light'); // Check whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
 
-  const showAlert =(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
-    })
-    setTimeout(()=> {
+    });
+    setTimeout(() => {
       setAlert(null);
     }, 2000);
-  }
+  };
 
-  const toggleMode = () =>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode Enabled!", "success");
-      document.title ="TextUtils - Dark Mode";
-      // setInterval(()=> {
-      //   document.title ="TextUtils is an Amazing Mode";
-      // },2000);
-      // setInterval(()=> {
-      //   document.title ="Download TextUtils Now";
-      // },2000);
-    }
-    else{
+      document.title = "TextUtils - Dark Mode";
+    } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode Enabled", "success");
-      document.title ="TextUtils - Light Mode";
-
-
+      document.title = "TextUtils - Light Mode";
     }
-  }
+  };
 
-  return (
-    <>
-      <Navbar title="TextUtils" about="About" contact="Contact" mode={mode}  toggleMode={toggleMode} />
-      {/* passing props */}
-      {/* <Navbar/> */}
+  return <BrowserRouter>
 
-      <Alert alert={alert}/>
+    <Navbar title="TextUtils" about="About" contact="Contact" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert} />
+    <div className="container">
+      <Routes>
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/" element={<TextFormss heading="Enter the text to analyze below:" mode={mode} showAlert={showAlert} />} />
+      </Routes>
+    </div>
+    {/* <About/> */}
+    {/* <TextForm/> */}
 
+  </BrowserRouter>
 
-      <div className="container">  
-        <TextForm heading="Enter the text to analyze below:" mode={mode} showAlert={showAlert} />
-      </div> 
-       
-     {/* <About/> */}
-
-
-
-
-    </>
-  );
 }
 
 export default App;
